d69d0100e836a3b9e6539ca0b72a789c9f719973
emilroenn <emilroenn@gmail.com>
2021-04-29 14:38:01 +0200

    Get covid data from data hub

---
+++ Get_covid_data.py
@@ -0,0 +1,26 @@
+
+#Imports
+from covid19dh import covid19
+from datetime import date
+
+#Get data from covid19dh and preprocess
+def get_data(country = "USA", level = 3, start = date(2020,1,1)):
+    #Get data function
+    x, src = covid19(country, level = level, start = start)
+    ##clean up
+    #Select columns:
+    x = x[[
+        'date',
+        'id',
+        'confirmed',
+        'deaths',
+        'population',
+        'administrative_area_level_1',
+        'administrative_area_level_2',
+        'administrative_area_level_3',
+        'latitude', 'longitude']]
+    
+    return x
+
+data = get_data()
+
