233fe92f54d0ee7e2d9b3d9ec3c2485c23ae0712
victor-m-p <victormoeller@gmail.com>
2021-05-10 17:48:48 +0200

    testing a lot of stuff

--- code/tmp_dim_mikkels.py
+++ code/tmp_dim_mikkels.py
@@ -12,6 +12,7 @@ import arviz as az
 import matplotlib.pyplot as plt
 import os 
 import theano
+import theano.tensor as tt 
 import random
 # import functions from fns file 
 import fns as f
@@ -22,15 +23,6 @@ df = get_data(level = 2, start = date(2020,1,1)) #can get more or less data here
 df["new_infected"] = df.groupby(["administrative_area_level_2"])["confirmed"].diff()
 df = df[df["administrative_area_level_2"].isin(["Colorado"])]
 df = df[df["new_infected"].notna()]
-
-def det_dot(a, b):
-    """
-    The theano dot product and NUTS sampler don't work with large matrices?
-    
-    :param a: (np matrix)
-    :param b: (theano vector)
-    """
-    return (a * b[None, :]).sum(axis=-1)
 
 df.reset_index(inplace = True)
 df['date'] = pd.to_datetime(df['date'])
@@ -51,149 +43,16 @@ def scalar(df, df_ref):
 train = scalar(train, train)
 test = scalar(test, train)
 
-t = train.t.values
-y_scaled = train.y_scaled.values
-n = 3
-p = 10
-
-## Fourier
-np.random.seed(6)
-def fourier_series(t, p=365.25, n=10):
-    # 2 pi n / p
-    x = 2 * np.pi * np.arange(1, n + 1) / p
-    # 2 pi n / p * t
-    x = x * t[:, None]
-    x = np.concatenate((np.cos(x), np.sin(x)), axis=1)
-    return x
-
-## Seasonality
-def seasonality_model(m, df, period='yearly', seasonality_prior_scale=10):
-    
-    if period == 'yearly':
-        n = 10
-        # rescale the period, as t is also scaled
-        p = 365.25 / (df['date'].max() - df['date'].min()).days
-        
-    if period == "monthly":
-        n = 10
-        # rescale the period, as t is also scaled
-        p = 30.5 / (df['date'].max() - df['date'].min()).days
-    else:  # weekly
-        n = 3
-        # rescale the period, as t is also scaled
-        p = 7 / (df['date'].max() - df['date'].min()).days
-    x = fourier_series(df['t'], p, n)
-    with m:
-        beta = pm.Normal(f'beta_{period}', mu=0, sd=seasonality_prior_scale, shape=2 * n)
-    return x, beta
-
-
-t = train.t.values
-y_scaled = train.y_scaled.values
-n = 3
-p = 10
-
-
-with pm.Model() as m:
-    # changepoints_prior_scale is None, so the exponential distribution
-    # will be used as prior on \tau.
-    #y, A, s = trend_model(m, df['t'], changepoints_prior_scale=None, n_changepoints = 40)
-    #x_yearly, beta_yearly = seasonality_model(m, df, 'yearly')
-    #t_shared = pm.Data('t_shared', t)
-    
-    '''
-    # 2 pi n / p
-    x = 2 * np.pi * np.arange(1, n + 1) / p
-    # 2 pi n / p * t
-    x = x * t[:, None]
-    x = np.concatenate((np.cos(x), np.sin(x)), axis=1)
-    '''
-    x = fourier_series()
-    
-    
-    sigma = pm.HalfCauchy('sigma', 0.5, testval=1)
-    y_pred = pm.Normal('y_pred', 
-                       mu = x,
-                       sd = sigma,
-                       observed = y_scaled)
-    #x_monthly, beta_monthly = seasonality_model(m, df, "monthly")
-    #x_weekly, beta_weekly = seasonality_model(m, df, 'weekly')
-    '''
-    x_monthly, beta_monthly = seasonality_model(m, df, "monthly")
-    x_weekly, beta_weekly = seasonality_model(m, df, 'weekly')
-    
-    y += det_dot(x_weekly, beta_weekly) + det_dot(x_monthly, beta_monthly) # + det_dot(x_yearly, beta_yearly)
-    
-    sigma = pm.HalfCauchy('sigma', 0.5, testval=1)
-    obs = pm.Normal('obs', 
-                 mu=y, 
-                 sd=sigma,
-                 observed=df['y_scaled'])
-    '''
-
-with m: 
-    m_idata = pm.sample(return_inferencedata=True)
-    
-m_idata
-
-##### Testing stuff ##### 
-arr1 = np.array([[5, 6, 7, 8]])
-arr2 = np.array([[1, 2, 3, 4]])
-
-import theano
-import theano.tensor as tt  
-import numpy as np
-a = tt.matrix('a')
-b = theano.shared(np.array([[3, 4, 5, 6]]))
-c = a + b
-f = theano.function(inputs = [a], outputs = [c])
-output = f(np.array([[3, 4, 5, 6]]))
-output
-
-##### testing Mikkel ##### 
-
-def fourier_series(t, p=365.25, n=10):
-    # 2 pi n / p
-    x = 2 * np.pi * np.arange(1, n + 1) / p
-    # 2 pi n / p * t
-    x = x * t[:, None]
-    x = np.concatenate((np.cos(x), np.sin(x)), axis=1)
-    return x
-
-## different versions of t. 
-t = train.t.values
-t_format = t[:, None]
-t_shared = theano.shared(t)
-t_format_shared = theano.shared(t_format)
-
-## testing it out. 
-p = theano.tensor.scalar('p')
-n = theano.tensor.scalar('n')
-c = pm.math.dot((2 * np.pi * tt.arange(1, n + 1) / p), t_format_shared)
-f = theano.function(inputs = [p, n], outputs = [c])
-out = f(365, 10)
-
 
 ## pyMC3
+n = 3
+p = 10
 c = (2 * np.pi * np.arange(1, n + 1) / p)[:, None]
 t = train.t.values
 y_scaled = train.y_scaled.values
-n = 3
-p = 10
 seasonality_prior_scale=10
 
-'''
-def det_dot(a, b):
-    """
-    The theano dot product and NUTS sampler don't work with large matrices?
-    
-    :param a: (np matrix)
-    :param b: (theano vector)
-    """
-    return (a * b[None, :]).sum(axis=-1)
-'''
 
-import theano.tensor as tt
 with pm.Model() as m2: 
     
     # shared 
