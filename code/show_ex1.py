2ac2e313f3644fb609a15b550e648d014e53b2d9
victor-m-p <victormoeller@gmail.com>
2021-05-24 15:06:07 +0200

    PyCipio3_VMP

--- code/show_ex1.py
+++ code/show_ex1.py
@@ -15,7 +15,7 @@ import theano
 import theano.tensor as tt 
 import random
 import fns as f
-import PyCipio3 as pc 
+import PyCipio3_VMP as pc 
 import pickle
 
 ## load data
@@ -30,72 +30,15 @@ sim.fit(p1 = (7, 1), p2 = (30, 1), p1_mode = "additive", p2_mode = "additive")
 ## load idata into the class
 sim.load_idata("../models/m_ex1_7-1_30-1")
 
-## now we can check stuff
-sim.plotting()
-sim.plot_fit_idx("group_one")
+## check that everything is good
+sim.plot_pp()
+sim.plot_trace()
 
-## predictions 
-sim.predict()
-sim.plot_predict_idx("group_one")
+## check fit 
+sim.plot_fit_idx(["group_one", "group_two"])
+sim.plot_fit_idx(["group_one"])
 
+## check predictions 
+sim.plot_predict_idx(["group_one", "group_two"])
+sim.plot_predict_idx(["group_one"])
 
-
-
-
-### plot just one group ###
-# 1. take out coords
-pp = sim.m_idata.posterior_predictive["y_pred"].mean(axis = 0)
-g1 = pp.sel(idx = "group_one")
-g1.shape
-
-m_pred_std = g1.std(axis = 0).data
-m_pred = g1.mean(axis = 0).data
-m_pred.shape
-idx_shared = sim.m_idata.constant_data.idx_shared
-t_shared = sim.m_idata.constant_data.t1_shared
-
-### plot several groups ### 
-all_g = pp.sortby(["idx"]).mean(axis = (0, 1)).data
-
-# this is needed
-test = pp.data
-test2 = np.reshape(test, (1000, 2, 43))
-means = test2.mean(axis = (0, 1))
-sds = test2.std(axis = (0, 1))
-
-plt.plot(range(0, 43), sds)
-
-idx_onex = ["group_one" for i in range(43)]
-idx_twox = ["group_two" for i in range(43)]
-idx_truex = np.append(idx_onex, idx_twox)
-
-
-for i in np.unique(idx_truex):
-    all_g[(idx_truex == i)]
-    np.mean()
-
-
-all_g
-### plot avg. of all groups ###
-
-
-
-
-# 2. take out posterior predictive 
-pp = sim.m_idata.posterior_predictive
-
-# 3. assign coords
-pp = pp.assign_coords(idx_shared = sim.idx_train)
-pp = pp.assign_coords(t_shared = sim.t1_train)
-
-sim.m_idata.posterior
-
-### predict
-sim.predict()
-
-
-### testing stuff
-idx = np.array([0, 0, 1, 1])
-val = np.array([0, 1, 2, 3])
-
-val[(idx == 1)]
